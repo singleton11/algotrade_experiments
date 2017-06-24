2a74c9effe367670f2507f61c2deefb5fb3fc07c
Anton Prokhorov <prokhorov@saritasa.com>
2017-06-24 17:52:56 +0700

    Extract `ConfigLoader` to separate module

--- algotrade.py
+++ algotrade.py
@@ -1,21 +1,6 @@
 import logging
-from typing import Dict, Tuple
 
-import yaml
-
-
-class ConfigLoader(object):
-    class Constants(object):
-        ENVIRONMENT: str = 'environment'
-        ACCESS_TOKEN: str = 'access_token'
-
-    @staticmethod
-    def load(file_name: str = 'key.yaml') -> Tuple[str, str]:
-        with open(file_name) as f:
-            settings: Dict[str, str] = yaml.load(f)
-            return (settings.get(ConfigLoader.Constants.ENVIRONMENT),
-                    settings.get(ConfigLoader.Constants.ACCESS_TOKEN))
-
+from config_loader.loader import ConfigLoader
 
 if __name__ == '__main__':
     environment, access_token = ConfigLoader.load()
---
+++ config_loader/loader.py
@@ -0,0 +1,27 @@
+from typing import Tuple, Dict
+
+import yaml
+
+
+class ConfigLoader(object):
+    """Config loader
+
+    Load config from yaml
+
+    Yaml example:
+        environment: 'practice'
+        access_token: 'this is token'
+
+    """
+
+    class Constants(object):
+        ENVIRONMENT: str = 'environment'
+        ACCESS_TOKEN: str = 'access_token'
+
+    @staticmethod
+    def load(file_name: str = 'key.yaml') -> Tuple[str, str]:
+        """Returns ``environment`` and ``access_token``"""
+        with open(file_name) as f:
+            settings: Dict[str, str] = yaml.load(f)
+            return (settings.get(ConfigLoader.Constants.ENVIRONMENT),
+                    settings.get(ConfigLoader.Constants.ACCESS_TOKEN))
