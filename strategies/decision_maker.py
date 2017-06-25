5f1c2a2e209c60abc94cad00135d3a3ced834283
Anton Prokhorov <prokhorov@saritasa.com>
2017-06-25 19:55:21 +0700

    Add `DO_NOTHING` constant to `DecisionMaker.Constants`

--- strategies/decision_maker.py
+++ strategies/decision_maker.py
@@ -6,14 +6,16 @@ class DecisionMaker(object):
     """Interface should every decision part of strategy implement"""
 
     class Constants(object):
+        DO_NOTHING = 'nothing'
         SELL: str = 'sell'
         BUY: str = 'buy'
         CLOSE: str = 'close'
 
-    def decide(self, df: pd.DataFrame, api: API) -> str:
+    def decide(self, df: pd.DataFrame, api: API, account_id: str) -> str:
         """Make a decision
 
         Args:
+            account_id: account id against which operation should be executed
             df: Historical data of trades
             api: API instance, was injected to be able to replace API by
                 simulation to make backtesting
